9079534e5686983845e280a0cdd18ccb6869df9e
Chmouel Boudjnah <chmouel@chmouel.com>
2023-02-21 22:48:55 +0100

    i am batman
    Signed-off-by: Chmouel Boudjnah <chmouel@chmouel.com>

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,7 @@ struct MounchConfig {
     icon: Option<String>,
     description: Option<String>,
 }
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize)]
 struct Config {
     #[serde(flatten)]
     toplevel: HashMap<String, Value>,
@@ -42,7 +42,6 @@ fn get_icon_path() -> HashMap<String, String> {
             icon_map.insert(fname, entry.path().to_str().unwrap().to_string());
         }
     }
-    // add ~/.local/share/icons
     let home = std::env::var("HOME").unwrap();
     for entry in walkdir::WalkDir::new(home + "/.local/share/icons")
         .into_iter()
@@ -56,7 +55,6 @@ fn get_icon_path() -> HashMap<String, String> {
             .next()
             .unwrap()
             .to_string();
-        // check if extension finishes by png or svg
         if entry.path().to_str().unwrap().ends_with("png")
             || entry.path().to_str().unwrap().ends_with("svg")
         {
@@ -67,48 +65,68 @@ fn get_icon_path() -> HashMap<String, String> {
 }
 
 // read configuration from a yaml file as argument
-fn read_config(filename: &str) -> String {
+fn read_config(filename: &str) -> Vec<MounchConfig> {
     let file = File::open(filename).expect("file not found");
     let config: Config = serde_yaml::from_reader(file).expect("cannot parse yaml");
-    let icon_map = get_icon_path();
-    let mut ret = String::new();
-    for (key, value) in config.toplevel {
+    let mut mounchconfigs: Vec<MounchConfig> = Vec::new();
+    for (_, value) in config.toplevel {
         if value.is_mapping() {
-            let mc: MounchConfig = serde_yaml::from_value(value).unwrap();
-            let s = mc.description.unwrap_or(key);
-            let icon = mc.icon.unwrap_or("default".to_string());
-            let icon_path = icon_map
-                .get(&icon)
-                .unwrap_or(&"default".to_string())
-                .to_string();
-            ret.push_str(&format!("img:{icon_path}:text:{s}\n"));
+            mounchconfigs.push(serde_yaml::from_value(value).unwrap());
         }
     }
-    ret
+    mounchconfigs
 }
 
 fn run_rofi_with_input(input: String) -> String {
+    let home = std::env::var("HOME").unwrap();
+    let xdg_cache_home = std::env::var("XDG_CACHE_HOME").unwrap_or(format!("{home}/.cache"));
     let mut child = Command::new("wofi")
-        .arg("-d -G -I --alow-images --allow-markup -W500 -H500 -i")
+        .arg(format!(
+            "-d -G -I -k {xdg_cache_home}/raffi --alow-images --allow-markup -W500 -H500 -i"
+        ))
         .stdout(Stdio::piped())
         .stdin(Stdio::piped())
+        .stderr(Stdio::null())
         .spawn()
         .expect("cannot launch wofi command");
 
-    // Write to stdin
     if let Some(stdin) = child.stdin.as_mut() {
         stdin.write_all(input.as_bytes()).unwrap();
     }
 
-    // Read stdout
     let output = child.wait_with_output().expect("failed to read output");
-    // String::from_utf8_lossy(output.stdout).to_string()
     String::from_utf8(output.stdout).unwrap()
+}
+
+fn make_wofi_input(mounchconfigs: &Vec<MounchConfig>) -> String {
+    let mut ret = String::new();
+    let icon_map = get_icon_path();
+    for mc in mounchconfigs {
+        let s = mc.description.clone().unwrap_or(mc.binary.clone());
+        let icon = mc.icon.clone().unwrap_or("default".to_string());
+        let icon_path = icon_map
+            .get(&icon)
+            .unwrap_or(&"default".to_string())
+            .to_string();
+        ret.push_str(&format!("img:{icon_path}:text:{s}\n",));
+    }
+    ret
 }
 
 fn main() {
     let home = std::env::var("HOME").unwrap();
-    let inputs = read_config(&(home + "/.config/mounch/mounch.yaml"));
+    let mounchconfigs = read_config(&(home + "/.config/mounch/mounch.yaml"));
+    let inputs = make_wofi_input(&mounchconfigs);
     let ret = run_rofi_with_input(inputs);
-    let chosen = "{:?}", ret.split(':').last().unwrap().trim();
+    let chosen = ret.split(':').last().unwrap().trim();
+    // match chosen in mounchconfigs
+    for mc in mounchconfigs {
+        if mc.description.unwrap_or(mc.binary.clone()) == chosen {
+            let mut child = Command::new(mc.binary)
+                .args(mc.args.unwrap_or(vec![]))
+                .spawn()
+                .expect("cannot launch binary");
+            child.wait().expect("cannot wait for child");
+        }
+    }
 }
