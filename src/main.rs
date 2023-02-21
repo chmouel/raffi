6d09a7740524be74503c718b798d59960dde7c13
Chmouel Boudjnah <chmouel@chmouel.com>
2023-02-21 22:01:57 +0100

    Using a nevergonnamakeyoucry

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,93 @@
 use std::{
+    collections::HashMap,
+    fs::File,
     io::Write,
     process::{Command, Stdio},
 };
 
-fn run_command_with_input(input: &[String]) -> String {
+use serde::Deserialize;
+use serde_yaml::Value;
+
+#[derive(Deserialize)]
+struct MounchConfig {
+    binary: String,
+    args: Option<Vec<String>>,
+    icon: Option<String>,
+    description: Option<String>,
+}
+#[derive(Deserialize, Debug)]
+struct Config {
+    #[serde(flatten)]
+    toplevel: HashMap<String, Value>,
+}
+
+fn get_icon_path() -> HashMap<String, String> {
+    // create a hasmap of icon names and paths
+    let mut icon_map = HashMap::new();
+    for entry in walkdir::WalkDir::new("/usr/share/icons")
+        .into_iter()
+        .filter_map(|e| e.ok())
+    {
+        let fname = entry
+            .file_name()
+            .to_str()
+            .unwrap()
+            .split('.')
+            .next()
+            .unwrap()
+            .to_string();
+        if entry.path().to_str().unwrap().ends_with("png")
+            || entry.path().to_str().unwrap().ends_with("svg")
+        {
+            icon_map.insert(fname, entry.path().to_str().unwrap().to_string());
+        }
+    }
+    // add ~/.local/share/icons
+    let home = std::env::var("HOME").unwrap();
+    for entry in walkdir::WalkDir::new(home + "/.local/share/icons")
+        .into_iter()
+        .filter_map(|e| e.ok())
+    {
+        let fname = entry
+            .file_name()
+            .to_str()
+            .unwrap()
+            .split('.')
+            .next()
+            .unwrap()
+            .to_string();
+        // check if extension finishes by png or svg
+        if entry.path().to_str().unwrap().ends_with("png")
+            || entry.path().to_str().unwrap().ends_with("svg")
+        {
+            icon_map.insert(fname, entry.path().to_str().unwrap().to_string());
+        }
+    }
+    icon_map
+}
+
+// read configuration from a yaml file as argument
+fn read_config(filename: &str) -> String {
+    let file = File::open(filename).expect("file not found");
+    let config: Config = serde_yaml::from_reader(file).expect("cannot parse yaml");
+    let icon_map = get_icon_path();
+    let mut ret = String::new();
+    for (key, value) in config.toplevel {
+        if value.is_mapping() {
+            let mc: MounchConfig = serde_yaml::from_value(value).unwrap();
+            let s = mc.description.unwrap_or(key);
+            let icon = mc.icon.unwrap_or("default".to_string());
+            let icon_path = icon_map
+                .get(&icon)
+                .unwrap_or(&"default".to_string())
+                .to_string();
+            ret.push_str(&format!("img:{icon_path}:text:{s}\n"));
+        }
+    }
+    ret
+}
+
+fn run_rofi_with_input(input: String) -> String {
     let mut child = Command::new("wofi")
         .arg("-d -G -I --alow-images --allow-markup -W500 -H500 -i")
         .stdout(Stdio::piped())
@@ -13,7 +97,7 @@ fn run_command_with_input(input: &[String]) -> String {
 
     // Write to stdin
     if let Some(stdin) = child.stdin.as_mut() {
-        stdin.write_all(input.join("\n").as_bytes()).unwrap();
+        stdin.write_all(input.as_bytes()).unwrap();
     }
 
     // Read stdout
@@ -23,11 +107,8 @@ fn run_command_with_input(input: &[String]) -> String {
 }
 
 fn main() {
-    let input = vec![
-        String::from("one"),
-        String::from("two"),
-        String::from("three"),
-    ];
-    let output = run_command_with_input(&input);
-    print!("{output}")
+    let home = std::env::var("HOME").unwrap();
+    let inputs = read_config(&(home + "/.config/mounch/mounch.yaml"));
+    let ret = run_rofi_with_input(inputs);
+    let chosen = "{:?}", ret.split(':').last().unwrap().trim();
 }
