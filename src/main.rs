9890de9e3a0cf1f4c56a5c8eabbc984b19c0c896
Chmouel Boudjnah <chmouel@chmouel.com>
2023-02-21 23:33:35 +0100

    rename and better
    Signed-off-by: Chmouel Boudjnah <chmouel@chmouel.com>

--- src/main.rs
+++ src/main.rs
@@ -9,72 +9,106 @@ use serde::Deserialize;
 use serde_yaml::Value;
 
 #[derive(Deserialize)]
-struct MounchConfig {
+struct RaffiConfig {
     binary: String,
     args: Option<Vec<String>>,
     icon: Option<String>,
     description: Option<String>,
+    ifenveq: Option<Vec<String>>,
+    ifenvset: Option<String>,
+    ifenvnotset: Option<String>,
+    ifexist: Option<String>,
+    disabled: Option<bool>,
 }
+
 #[derive(Deserialize)]
 struct Config {
     #[serde(flatten)]
     toplevel: HashMap<String, Value>,
 }
 
-fn get_icon_path() -> HashMap<String, String> {
+fn get_icon_map() -> HashMap<String, String> {
     // create a hasmap of icon names and paths
     let mut icon_map = HashMap::new();
-    for entry in walkdir::WalkDir::new("/usr/share/icons")
-        .into_iter()
-        .filter_map(|e| e.ok())
-    {
-        let fname = entry
-            .file_name()
-            .to_str()
-            .unwrap()
-            .split('.')
-            .next()
-            .unwrap()
-            .to_string();
-        if entry.path().to_str().unwrap().ends_with("png")
-            || entry.path().to_str().unwrap().ends_with("svg")
+    let iconhome = std::env::var("XDG_DATA_HOME")
+        .unwrap_or(format!("{}/.local/share", std::env::var("HOME").unwrap()))
+        + "/icons";
+    let icondirs = vec!["/usr/share/icons", "/usr/share/pixmaps", iconhome.as_str()];
+    for dir in icondirs {
+        for entry in walkdir::WalkDir::new(dir)
+            .into_iter()
+            .filter_map(|e| e.ok())
         {
-            icon_map.insert(fname, entry.path().to_str().unwrap().to_string());
-        }
-    }
-    let home = std::env::var("HOME").unwrap();
-    for entry in walkdir::WalkDir::new(home + "/.local/share/icons")
-        .into_iter()
-        .filter_map(|e| e.ok())
-    {
-        let fname = entry
-            .file_name()
-            .to_str()
-            .unwrap()
-            .split('.')
-            .next()
-            .unwrap()
-            .to_string();
-        if entry.path().to_str().unwrap().ends_with("png")
-            || entry.path().to_str().unwrap().ends_with("svg")
-        {
-            icon_map.insert(fname, entry.path().to_str().unwrap().to_string());
+            let fname = entry
+                .file_name()
+                .to_str()
+                .unwrap()
+                .split('.')
+                .next()
+                .unwrap()
+                .to_string();
+            if entry.path().to_str().unwrap().ends_with("png")
+                || entry.path().to_str().unwrap().ends_with("svg")
+            {
+                icon_map.insert(fname, entry.path().to_str().unwrap().to_string());
+            }
         }
     }
     icon_map
 }
 
-// read configuration from a yaml file as argument
-fn read_config(filename: &str) -> Vec<MounchConfig> {
-    let file = File::open(filename).expect("file not found");
+// read configuration from a yaml according to the if conditions
+fn read_config(filename: &str) -> Vec<RaffiConfig> {
+    let file =
+        File::open(filename).unwrap_or_else(|_| panic!("cannot open config file {filename}"));
     let config: Config = serde_yaml::from_reader(file).expect("cannot parse yaml");
-    let mut mounchconfigs: Vec<MounchConfig> = Vec::new();
+    let mut rafficonfigs: Vec<RaffiConfig> = Vec::new();
     for (_, value) in config.toplevel {
         if value.is_mapping() {
-            mounchconfigs.push(serde_yaml::from_value(value).unwrap());
+            let mc: RaffiConfig = serde_yaml::from_value(value).unwrap();
+            if let Some(disabled) = mc.disabled {
+                if disabled {
+                    continue;
+                }
+            }
+            if let Some(ifenveq) = mc.ifenveq.clone() {
+                if ifenveq.len() != 2 {
+                    continue;
+                }
+                if std::env::var(&ifenveq[0]).unwrap_or("".to_string()) != ifenveq[1] {
+                    continue;
+                }
+            }
+            if let Some(ifenvset) = mc.ifenvset.clone() {
+                if std::env::var(&ifenvset).is_ok() {
+                    continue;
+                }
+            }
+
+            if let Some(ifenvnotset) = mc.ifenvnotset.clone() {
+                if std::env::var(&ifenvnotset).is_err() {
+                    continue;
+                }
+            }
+
+            if let Some(ifexist) = mc.ifexist.clone() {
+                // search ifexist in PATH
+                let paths = std::env::var("PATH").unwrap();
+                let mut found = false;
+                for path in paths.split(':') {
+                    if std::path::Path::new(&(path.to_string() + "/" + &ifexist)).exists() {
+                        found = true;
+                        break;
+                    }
+                }
+                if !found {
+                    continue;
+                }
+            }
+            rafficonfigs.push(mc);
         }
     }
-    mounchconfigs
+    rafficonfigs
 }
 
 fn run_rofi_with_input(input: String) -> String {
@@ -98,16 +132,20 @@ fn run_rofi_with_input(input: String) -> String {
     String::from_utf8(output.stdout).unwrap()
 }
 
-fn make_wofi_input(mounchconfigs: &Vec<MounchConfig>) -> String {
+fn make_wofi_input(rafficonfigs: &Vec<RaffiConfig>) -> String {
     let mut ret = String::new();
-    let icon_map = get_icon_path();
-    for mc in mounchconfigs {
+    let icon_map = get_icon_map();
+    for mc in rafficonfigs {
         let s = mc.description.clone().unwrap_or(mc.binary.clone());
-        let icon = mc.icon.clone().unwrap_or("default".to_string());
-        let icon_path = icon_map
+        let icon = mc.icon.clone().unwrap_or(mc.binary.clone());
+        let mut icon_path = icon_map
             .get(&icon)
             .unwrap_or(&"default".to_string())
             .to_string();
+        // check if icon exists
+        if std::path::Path::new(&icon).exists() {
+            icon_path = icon;
+        }
         ret.push_str(&format!("img:{icon_path}:text:{s}\n",));
     }
     ret
@@ -115,12 +153,12 @@ fn make_wofi_input(mounchconfigs: &Vec<MounchConfig>) -> String {
 
 fn main() {
     let home = std::env::var("HOME").unwrap();
-    let mounchconfigs = read_config(&(home + "/.config/mounch/mounch.yaml"));
-    let inputs = make_wofi_input(&mounchconfigs);
+    let xdg_config_home = std::env::var("XDG_CONFIG_HOME").unwrap_or(format!("{home}/.config"));
+    let rafficonfigs = read_config(&(xdg_config_home + "/raffi/raffi.yaml"));
+    let inputs = make_wofi_input(&rafficonfigs);
     let ret = run_rofi_with_input(inputs);
     let chosen = ret.split(':').last().unwrap().trim();
-    // match chosen in mounchconfigs
-    for mc in mounchconfigs {
+    for mc in rafficonfigs {
         if mc.description.unwrap_or(mc.binary.clone()) == chosen {
             let mut child = Command::new(mc.binary)
                 .args(mc.args.unwrap_or(vec![]))
